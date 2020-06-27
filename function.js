8357403308e62dad191fad73ec1ac37d8dfd5508
singhanujd <singhanujd@gmail.com>
2020-06-28 00:24:22 +0530

    Functions

---
+++ function.js
@@ -0,0 +1,13 @@
+//create
+
+let fn1 = function(){
+  return 10;
+}
+
+function fn2(){
+  return 20;
+}
+
+//invoke
+console.log(fn1());
+console.log(fn2());
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Import stylesheets
 import './style.css';
 // import './variables.js';
-import './object.js'
+// import './object.js';
+import './function.js';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
