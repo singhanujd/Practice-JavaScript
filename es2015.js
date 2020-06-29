333b0c75263372f8aa2316e844101e2e9ce3bb11
singhanujd <singhanujd@gmail.com>
2020-06-29 21:08:21 +0530

    ES6

--- es2015.js
+++ es2015.js
@@ -1,13 +1,40 @@
-//Block level scoping
-function sayHi(){
-  var PI = 3.14;
-  var name = 'Jitendra Singh';
-  // console.log(PI,name);
+// const
+// let
+
+//for..of loop
+let arr = [6,7,8,9];
+for(let item of arr){
+  console.log(item);
 }
-// {
-//   const PI = 3.14;
-//   let name = 'Jitendra Singh';
-//   console.log(PI,name);
-// }
-sayHi();
-console.log(PI,name);
+
+//string templates
+let name = 'Jitendra Singh';
+let age = 29;
+console.log(`My name is ${name} and My age is ${age}`);
+
+//Object, Array Destructuring and default parameter
+let arr1 = [1,2,3,[4,5,6,[7,8]]];
+let obj1 = {
+  name: 'Jitendra Singh',
+  age: 29,
+  address: {
+    city: 'Hyderabad'
+  }
+}
+
+let [a,b,c,[d,e,f,[g,h,i = 10]]] = arr1;
+let {name:myName, age, address: { country = 'India',city : myCity}} = obj1;
+console.log(a,b,c,d,e,f,g,h,i);
+console.log(myName,age,myCity,country);
+
+//spread operator
+console.log([...arr1]);
+console.log({...obj1});
+
+//arrow function
+//does not have it's own this keyword. Inherit from parent
+
+let sayName = () => {
+  console.log(this.name);
+}
+console.log(sayName());
